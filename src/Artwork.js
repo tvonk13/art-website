b8670eaa3ba56bcbd9021174251a85da318c13e5
tvonk13 <taylor.c.v@gmail.com>
2020-07-11 20:30:56 -0700

    Cleaned up artwork code

--- src/Artwork.js
+++ src/Artwork.js
@@ -27,8 +27,8 @@ const useStyles = makeStyles(theme => ({
         marginBottom: theme.spacing(1),
     },
     img: {
-        //maxHeight: '100%',
-        //maxWidth: '80%',
+        maxHeight: '100%',
+        maxWidth: '80%',
         marginRight: theme.spacing(6),
         marginLeft: theme.spacing(10),
         boxShadow: "5px 5px 15px 0 #f0f0f0",
@@ -40,39 +40,26 @@ export default function Artwork(props) {
     const id = props.match.params.id;
 
     const [isImgLoaded, setIsImgLoaded] = useState(false);
-    const [imgWidth, setImageWidth] = useState(0);
-    const [imgHeight, setImageHeight] = useState(0);
 
     useEffect(() => {
         var img = new Image();
         img.onload = () => {
             setIsImgLoaded(true);
         }
-        var scale = .15;
         img.src = data[id].img;
-        setImageWidth(img.naturalWidth * scale)
-        setImageHeight(img.naturalHeight * scale)
-        console.log(img.naturalWidth + " -> " + img.naturalWidth * scale)
-        console.log(img.naturalHeight + " -> " + img.naturalHeight * scale)
     })
 
     return (
+        <Fade in={isImgLoaded} timeout={1000}>
         <div className={classes.artworkContainer}>
-            {
-                data[id]
-                ? <>
-                    <Fade in={isImgLoaded} timeout={1000}>
-                        <img src={data[id].img} alt={data[id].title} style={{width: imgWidth, height: imgHeight}} className={classes.img} />
-                    </Fade>
-                    <div className={classes.description}>
-                        <Typography variant="h4" className={classes.title}>{data[id].title}</Typography>
-                        <Typography variant="body2" className={classes.medium}><i>{data[id].medium}</i></Typography>
-                        { data[id].description && <Typography variant="subtitle1">{data[id].description}</Typography> }
-                    </div>
-                </>
-                : "Image not found"
-            }
-
+            <img src={data[id].img} alt={data[id].title} className={classes.img} />
+            <div className={classes.description}>
+                <Typography variant="h4" className={classes.title}>{data[id].title}</Typography>
+                <Typography variant="body2" className={classes.medium}><i>{data[id].medium}</i></Typography>
+                { data[id].description && <Typography variant="subtitle1">{data[id].description}</Typography> }
+            </div>
         </div>
+        </Fade>
+
     )
 }
