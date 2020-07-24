a0054d8c9690d7be3ede73c91a8130ea7a37701b
tvonk13 <taylor.c.v@gmail.com>
2020-07-23 22:27:57 -0700

    Missed a file in commit

--- src/assets/photography.js
+++ src/assets/photography.js
@@ -1,43 +1,43 @@
-import Biarritz from './photography/biarritz.jpg';
-import BiarritzThumb from './photography/biarritz-thumb.jpg';
-import Bubbles from './photography/bubbles.jpg';
-import BubblesThumb from './photography/bubbles-thumb.jpg';
-import BuddhaPark from './photography/buddha-park.jpg';
-import BuddhaParkThumb from './photography/buddha-park-thumb.jpg';
-import BuddhaParkBW from './photography/buddha-park-bw.jpg';
-import BuddhaParkBWThumb from './photography/buddha-park-bw-thumb.jpg';
-import Bumblebee from './photography/bumblebee.JPG';
-import BumblebeeThumb from './photography/bumblebee-thumb.JPG';
-import Butterfly from './photography/butterfly.jpeg';
-import ButterflyThumb from './photography/butterfly-thumb.jpeg';
-import GoatRock from './photography/goat-rock.jpg';
-import GoatRockThumb from './photography/goat-rock-thumb.jpg';
-import GoldenGateBridge from './photography/golden-gate-bridge.jpg';
-import GoldenGateBridgeThumb from './photography/golden-gate-bridge-thumb.jpg';
-import GoldenGateBridgeBW from './photography/golden-gate-bridge-bw.jpg';
-import GoldenGateBridgeBWThumb from './photography/golden-gate-bridge-bw-thumb.jpg';
-import HalfDome from './photography/half-dome.jpg';
-import HalfDomeThumb from './photography/half-dome-thumb.jpg';
-import HalfDomeCliff from './photography/half-dome-cliff.jpg';
-import HalfDomeCliffThumb from './photography/half-dome-cliff-thumb.jpg';
-import HoleInTheWall from './photography/hole-in-the-wall.jpg';
-import HoleInTheWallThumb from './photography/hole-in-the-wall-thumb.jpg';
-import KuangSiFalls from './photography/kuang-si-falls.jpg';
-import KuangSiFallsThumb from './photography/kuang-si-falls-thumb.jpg';
-import TahoeSnow from './photography/tahoe-snow-fading.jpg';
-import TahoeSnowThumb from './photography/tahoe-snow-fading-thumb.jpg';
-import TahoeSunset from './photography/tahoe-sunset.jpg';
-import TahoeSunsetThumb from './photography/tahoe-sunset-thumb.jpg';
-import TreesMoss from './photography/trees-moss-contrast.jpg';
-import TreesMossThumb from './photography/trees-moss-contrast-thumb.jpg';
-import TwinTowers from './photography/twin-towers-memorial.jpeg';
-import TwinTowersThumb from './photography/twin-towers-memorial-thumb.jpeg';
+import Biarritz from "./photography/biarritz.jpg";
+import BiarritzThumb from "./photography/biarritz-thumb.jpg";
+import Bubbles from "./photography/bubbles.jpg";
+import BubblesThumb from "./photography/bubbles-thumb.jpg";
+import BuddhaPark from "./photography/buddha-park.jpg";
+import BuddhaParkThumb from "./photography/buddha-park-thumb.jpg";
+import BuddhaParkBW from "./photography/buddha-park-bw.jpg";
+import BuddhaParkBWThumb from "./photography/buddha-park-bw-thumb.jpg";
+import Bumblebee from "./photography/bumblebee.JPG";
+import BumblebeeThumb from "./photography/bumblebee-thumb.JPG";
+import Butterfly from "./photography/butterfly.jpeg";
+import ButterflyThumb from "./photography/butterfly-thumb.jpeg";
+import GoatRock from "./photography/goat-rock.jpg";
+import GoatRockThumb from "./photography/goat-rock-thumb.jpg";
+import GoldenGateBridge from "./photography/golden-gate-bridge.jpg";
+import GoldenGateBridgeThumb from "./photography/golden-gate-bridge-thumb.jpg";
+import GoldenGateBridgeBW from "./photography/golden-gate-bridge-bw.jpg";
+import GoldenGateBridgeBWThumb from "./photography/golden-gate-bridge-bw-thumb.jpg";
+import HalfDome from "./photography/half-dome.jpg";
+import HalfDomeThumb from "./photography/half-dome-thumb.jpg";
+import HalfDomeCliff from "./photography/half-dome-cliff.jpg";
+import HalfDomeCliffThumb from "./photography/half-dome-cliff-thumb.jpg";
+import HoleInTheWall from "./photography/hole-in-the-wall.jpg";
+import HoleInTheWallThumb from "./photography/hole-in-the-wall-thumb.jpg";
+import KuangSiFalls from "./photography/kuang-si-falls.jpg";
+import KuangSiFallsThumb from "./photography/kuang-si-falls-thumb.jpg";
+import TahoeSnow from "./photography/tahoe-snow-fading.jpg";
+import TahoeSnowThumb from "./photography/tahoe-snow-fading-thumb.jpg";
+import TahoeSunset from "./photography/tahoe-sunset.jpg";
+import TahoeSunsetThumb from "./photography/tahoe-sunset-thumb.jpg";
+import TreesMoss from "./photography/trees-moss-contrast.jpg";
+import TreesMossThumb from "./photography/trees-moss-contrast-thumb.jpg";
+import TwinTowers from "./photography/twin-towers-memorial.jpeg";
+import TwinTowersThumb from "./photography/twin-towers-memorial-thumb.jpeg";
 
 const biarritz = {
     img: Biarritz,
     thumb: BiarritzThumb,
-    title: 'Sunset In Biarritz',
-    id: 'biarritz',
+    title: "Sunset In Biarritz",
+    id: "biarritz",
     description: "Taken while jogging along the coast of Biarritz",
     subtitle: "Biarritz, France",
 }
@@ -45,16 +45,16 @@ const biarritz = {
 const bubbles = {
     img: Bubbles,
     thumb: BubblesThumb,
-    title: 'Bubbles',
-    id: 'bubbles',
+    title: "Bubbles",
+    id: "bubbles",
     description: "",
 }
 
 const buddhaPark = {
     img: BuddhaPark,
     thumb: BuddhaParkThumb,
-    title: 'Buddha In The Sky',
-    id: 'buddhaPark',
+    title: "Buddha In The Sky",
+    id: "buddhaPark",
     description: "",
     subtitle: "Buddha Park - Vientiane, Laos",
 }
@@ -62,8 +62,8 @@ const buddhaPark = {
 const buddhaParkBW = {
     img: BuddhaParkBW,
     thumb: BuddhaParkBWThumb,
-    title: 'Buddha In The Sky',
-    id: 'buddhaParkBW',
+    title: "Buddha In The Sky",
+    id: "buddhaParkBW",
     description: "",
     subtitle: "Buddha Park - Vientiane, Laos",
 }
@@ -71,24 +71,24 @@ const buddhaParkBW = {
 const bumblebee = {
     img: Bumblebee,
     thumb: BumblebeeThumb,
-    title: 'Bumblebee',
-    id: 'bumblebee',
+    title: "Bumblebee",
+    id: "bumblebee",
     description: "",
 }
 
 const butterfly = {
     img: Butterfly,
     thumb: ButterflyThumb,
-    title: 'Butterfly',
-    id: 'butterfly',
+    title: "Butterfly",
+    id: "butterfly",
     description: "",
 }
 
 const goatRock = {
     img: GoatRock,
     thumb: GoatRockThumb,
-    title: 'Goat Rock',
-    id: 'goatRock',
+    title: "Goat Rock",
+    id: "goatRock",
     description: "",
     subtitle: "Goat Rock - Jenner, CA",
 }
@@ -96,8 +96,8 @@ const goatRock = {
 const goldenGateBride = {
     img: GoldenGateBridge,
     thumb: GoldenGateBridgeThumb,
-    title: 'Golden Gate Bridge',
-    id: 'goldenGateBride',
+    title: "Golden Gate Bridge",
+    id: "goldenGateBride",
     description: "",
     subtitle: "Land's End Trail - San Francisco, CA",
 }
@@ -105,8 +105,8 @@ const goldenGateBride = {
 const goldenGateBrideBW = {
     img: GoldenGateBridgeBW,
     thumb: GoldenGateBridgeBWThumb,
-    title: 'Golden Gate Bridge',
-    id: 'goldenGateBrideBW',
+    title: "Golden Gate Bridge",
+    id: "goldenGateBrideBW",
     description: "",
     subtitle: "Land's End Trail - San Francisco, CA",
 }
@@ -114,8 +114,8 @@ const goldenGateBrideBW = {
 const halfDome = {
     img: HalfDome,
     thumb: HalfDomeThumb,
-    title: 'Half Dome',
-    id: 'halfDome',
+    title: "Half Dome",
+    id: "halfDome",
     description: "",
     subtitle: "Half Dome - Yosemite Valley, CA",
 }
@@ -123,8 +123,8 @@ const halfDome = {
 const halfDomeCliff = {
     img: HalfDomeCliff,
     thumb: HalfDomeCliffThumb,
-    title: 'Standing On Top of the World',
-    id: 'halfDomeCliff',
+    title: "Standing On Top of the World",
+    id: "halfDomeCliff",
     description: "",
     subtitle: "Half Dome - Yosemite Valley, CA",
 }
@@ -132,8 +132,8 @@ const halfDomeCliff = {
 const holeInTheWall = {
     img: HoleInTheWall,
     thumb: HoleInTheWallThumb,
-    title: 'Hole In The Wall',
-    id: 'holeInTheWall',
+    title: "Hole In The Wall",
+    id: "holeInTheWall",
     description: "",
     subtitle: "Roxbury, MA",
 }
@@ -141,8 +141,8 @@ const holeInTheWall = {
 const kuangSiFalls = {
     img: KuangSiFalls,
     thumb: KuangSiFallsThumb,
-    title: 'Kuang Si Falls',
-    id: 'kuangSiFalls',
+    title: "Kuang Si Falls",
+    id: "kuangSiFalls",
     description: "",
     subtitle: "Kuang Si Falls - Luang Prabang, Laos",
 }
@@ -150,8 +150,8 @@ const kuangSiFalls = {
 const tahoeSnow = {
     img: TahoeSnow,
     thumb: TahoeSnowThumb,
-    title: 'Snow Fading',
-    id: 'tahoeSnow',
+    title: "Snow Fading",
+    id: "tahoeSnow",
     description: "",
     subtitle: "Tahoe National Forrest - Soda Springs, CA",
 }
@@ -159,8 +159,8 @@ const tahoeSnow = {
 const tahoeSunset = {
     img: TahoeSunset,
     thumb: TahoeSunsetThumb,
-    title: 'Tahoe Sunset',
-    id: 'tahoeSunset',
+    title: "Tahoe Sunset",
+    id: "tahoeSunset",
     description: "",
     subtitle: "Rucker Lake, Tahoe National Forrest - Rucker Lake, CA",
 }
@@ -168,8 +168,8 @@ const tahoeSunset = {
 const treeMoss = {
     img: TreesMoss,
     thumb: TreesMossThumb,
-    title: 'Moss',
-    id: 'treeMoss',
+    title: "Moss",
+    id: "treeMoss",
     description: "",
     subtitle: "Tahoe National Forrest - Soda Springs, CA",
 }
@@ -177,8 +177,8 @@ const treeMoss = {
 const twinTowers = {
     img: TwinTowers,
     thumb: TwinTowersThumb,
-    title: 'Reflections',
-    id: 'twinTowers',
+    title: "Reflections",
+    id: "twinTowers",
     description: "Taken at the base of the Twin Towers Memorial",
     subtitle: "Twin Towers Memorial - New York, NY",
 }
