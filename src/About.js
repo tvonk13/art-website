7c24cf00cae9ca5c25746fff488f01d9b073aa9f
tvonk13 <taylor.c.v@gmail.com>
2020-12-17 18:34:03 -0800

    Reworked about page

--- src/About.js
+++ src/About.js
@@ -1,18 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles, Typography, Box, Grid, Paper, Fade } from "@material-ui/core";
+import { makeStyles, Typography, Box, Grid, Fade } from "@material-ui/core";
 import Prismic from 'prismic-javascript';
 import Client from './prismic-configuration';
 import { RichText } from 'prismic-reactjs'
 
 const useStyles = makeStyles(theme => ({
+    aboutContainer: {
+      marginTop: theme.spacing(5),
+    },
     profile: {
-        width: '200px',
-        height: '200px',
-        margin: theme.spacing(1),
+        width: 'auto',
+        height: '400px',
+        margin: theme.spacing(1, 3, 0, 0),
+        [theme.breakpoints.down('sm')]: {
+            justifyContent: 'center',
+            margin: theme.spacing(0),
+            width: '50%',
+            height: 'auto',
+        },
+    },
+    profileImageContainer: {
+        justifyContent: 'flex-end',
+        [theme.breakpoints.down('sm')]: {
+            justifyContent: 'center',
+        },
+    },
+    typographyContainer: {
+        margin: theme.spacing(0, 0, 0, 3),
+        [theme.breakpoints.down('sm')]: {
+            margin: theme.spacing(5, 5),
+        },
     },
     typography: {
-        marginBottom: theme.spacing(2),
         maxWidth: theme.spacing(80),
+        [theme.breakpoints.down('sm')]: {
+            maxWidth: '100%',
+        },
         fontSize: '1rem',
         lineHeight: '1.5',
         '& a':{
@@ -29,41 +52,44 @@ const useStyles = makeStyles(theme => ({
 export default function About() {
     const classes = useStyles();
 
-    const [isContentLoaded, setIsContentLoaded] = useState(false);
     const [about, setAbout] = useState(null);
 
     useEffect(() => {
         Client.query(
-            Prismic.Predicates.at('document.type', 'profile')
+            Prismic.Predicates.at('document.type', 'about')
         ).then(response => {
             setAbout(response.results[0]);
-            setIsContentLoaded(true);
         });
-    }, [])
+    }, []);
+
+    // Add target=_blank attr to each of the links in the description
+    useEffect(() => {
+        const description = document.getElementById('description');
+        if (description) {
+            Array.prototype.slice.call(description.getElementsByTagName('a'))
+                .forEach(a => {a.setAttribute('target', '_blank')})
+        }
+    }, [about]);
 
     return(
         <>
             {
                 about &&
-                <Grid container justify="center" style={{margin: '40px 0'}}>
-                    <Fade in={isContentLoaded} timeout={1000}>
-                        <Paper elevation={2}>
-                            <Grid container direction="column" alignItems="center" style={{padding: '20px 40px'}}>
-                                <Grid item>
-                                    <img src={about.data.image.url} alt="profile" className={classes.profile}/>
-                                </Grid>
-                                <Grid item container direction="column" alignItems="center" >
-                                    <Typography variant="h3" color="primary">Hi, I'm Taylor</Typography>
-                                    <Grid item container direction="column">
-                                        <Box className={classes.typography} color="primary">
-                                            {RichText.render(about.data.description)}
-                                        </Box>
-                                    </Grid>
-                                </Grid>
+                <Fade in={about} timeout={500}>
+                    <Grid container justify="center" className={classes.aboutContainer}>
+                        <Grid item xs={12} md={5} lg={4} container className={classes.profileImageContainer}>
+                                <img src={about.data.image.url} alt="profile" className={classes.profile}/>
+                        </Grid>
+                        <Grid item xs={12} md={5} lg={4} container direction="column" className={classes.typographyContainer}>
+                            <Typography variant="h3" color="primary">Hi, I'm Taylor</Typography>
+                            <Grid item container direction="column">
+                                <Box id='description' className={classes.typography} color="primary">
+                                    {RichText.render(about.data.description)}
+                                </Box>
                             </Grid>
-                        </Paper>
-                    </Fade>
-                </Grid>
+                        </Grid>
+                    </Grid>
+                </Fade>
             }
         </>
     )
--- src/App.js
+++ src/App.js
@@ -1,54 +1,32 @@
 import React from "react";
-import { Route, useLocation } from "react-router-dom";
-import { makeStyles, Grid } from "@material-ui/core";
+import { Route } from "react-router-dom";
+import { Grid } from "@material-ui/core";
 import Nav from "./Nav";
 import Art from "./Art";
 import Photography from './Photography';
 import About from "./About";
 import ImageViewer from "./ImageViewer";
-import Background from './assets/background.svg'
-import clsx from "clsx";
 import Doodles from './Doodles';
 
-const useStyles = makeStyles(theme => ({
-    main: {
-        display: "flex",
-        flexDirection: "row",
-        width: "100%",
-        minHeight: "100vh",
-        backgroundImage: `url(${Background})`,
-        backgroundSize: 'cover',
-        backgroundPositionY: '50%',
-    },
-    noBackground: {
-        backgroundImage: 'none'
-    }
-}));
-
 function App() {
-    const classes = useStyles();
-    const {pathname} = useLocation();
-
     return (
-        <div className={clsx(classes.main, pathname !== '/about' && classes.noBackground)}>
-            <Grid container direction="column" justify="flex-start">
-                <Grid item>
-                    <Nav />
-                </Grid>
-                <Grid item>
-                    <main>
-                        <Route exact path="/" component={Art} />
-                        <Route exact path="/art" component={Art} />
-                        <Route exact path="/doodles" component={Doodles} />
-                        <Route exact path="/photography" component={Photography} />
-                        <Route exact path="/art/:id" render={(props) => <ImageViewer {...props} />} />
-                        <Route exact path="/doodles/:id" render={(props) => <ImageViewer {...props} />} />
-                        <Route exact path="/photography/:id" render={(props) => <ImageViewer {...props} />} />
-                        <Route exact path="/about" component={About} />
-                    </main>
-                </Grid>
+        <Grid container direction="column" justify="flex-start">
+            <Grid item>
+                <Nav />
             </Grid>
-        </div>
+            <Grid item>
+                <main>
+                    <Route exact path="/" component={Art} />
+                    <Route exact path="/art" component={Art} />
+                    <Route exact path="/doodles" component={Doodles} />
+                    <Route exact path="/photography" component={Photography} />
+                    <Route exact path="/art/:id" render={(props) => <ImageViewer {...props} />} />
+                    <Route exact path="/doodles/:id" render={(props) => <ImageViewer {...props} />} />
+                    <Route exact path="/photography/:id" render={(props) => <ImageViewer {...props} />} />
+                    <Route exact path="/about" component={About} />
+                </main>
+            </Grid>
+        </Grid>
     );
 }
 
